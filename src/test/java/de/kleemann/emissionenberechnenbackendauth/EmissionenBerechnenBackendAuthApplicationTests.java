a4ce599703f56ec2d0366e97591f054ce1e8e220
Matteo Kleemann <117347339+matteoklee@users.noreply.github.com>
2024-12-17 23:04:54 +0100

    initial commit

---
+++ src/test/java/de/kleemann/emissionenberechnenbackendauth/EmissionenBerechnenBackendAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kleemann.emissionenberechnenbackendauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmissionenBerechnenBackendAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
